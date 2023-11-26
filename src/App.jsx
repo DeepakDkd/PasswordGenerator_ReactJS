0b658c53e330354630e62339b7ef3810d54b65f5
Deepak Dhakad <deepakdkd1188@gmail.com>
2023-11-27 00:15:46 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 
 
 function App() {
-  const [length, setlength] = useState(10);
+  const [length, setlength] = useState(8);
+  const [password, setpassword] = useState('');
   const [number, setnumber] = useState(false);
   const [char, setchar] = useState(false);
+  
   const refElem = useRef(null);
-  const [password, setpassword] = useState('');
+
   const generatePassword = useCallback(() => {
+
     let pass = '';
     let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     if (number) {
@@ -24,32 +27,33 @@ function App() {
     setpassword(pass);
   }, [length, number, char, setpassword]);
 
-useEffect(generatePassword,[length,number,char,setpassword]);
+
+  useEffect(generatePassword, [length, number, char, setpassword]);
 
 
   return (
     <>
       <main>
         <div className="card">
-        <h1>Password Generator</h1>
-        <div class='inp'>
-          <input type="text" value={password} ref={refElem} readOnly />
-          <i class="ri-file-copy-line" onClick={()=>{
-            refElem.current.select();
-            window.navigator.clipboard.writeText(password);
-          }}></i>
-        </div>  
-        <div className="len">
-          <input type="range" min={10} max={32} value={length} onChange={(e) => { setlength(e.target.value) }} />
-          <p>Length : {length}</p>
-        </div>
-        <div className="Check">
-          <input type="checkbox" onChange={() => { setnumber((prev => !prev)) }} />
-          <p>Number</p>
-          <input type="checkbox" onChange={() => { setchar((prev => !prev)) }} />
-          <p>Character</p>
-        </div>
-        <p class="note"><span>Note: </span>Always use Numbers & Characters for strong password.</p>
+          <h1>Password Generator</h1>
+          <div class='inp'>
+            <input type="text" value={password} ref={refElem} readOnly />
+            <i class="ri-file-copy-line" onClick={() => {
+              refElem.current.select();
+              window.navigator.clipboard.writeText(password);
+            }}></i>
+          </div>
+          <div className="len">
+            <input type="range" min={8} max={32} value={length} onChange={(e) => { setlength(e.target.value) }} />
+            <p>Length : {length}</p>
+          </div>
+          <div className="Check">
+            <input type="checkbox" onChange={() => { setnumber((prev => !prev)) }} />
+            <p>Number</p>
+            <input type="checkbox" onChange={() => { setchar((prev => !prev)) }} />
+            <p>Character</p>
+          </div>
+          <p class="note"><span>Note: </span>Always use Numbers & Characters for strong password.</p>
         </div>
       </main>
     </>
